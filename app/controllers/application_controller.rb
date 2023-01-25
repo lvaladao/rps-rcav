122d84a0648f96cd8eb2db001b50b4eef5942430
lvmadoreira <lvaladao@chicagobooth.edu>
2023-01-25 03:55:17 +0000

    Finished project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -8,21 +8,30 @@ class ApplicationController < ActionController::Base
     render({ :template => "game_templates/rules.html.erb" })
   end
 
+  def outcome(user_move)
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if user_move == @comp_move
+      @outcome = "tied"
+    elsif (user_move == "rock" && @comp_move == "scissors") || (user_move == "paper" && @comp_move == "rock") || (user_move == "scissors" && @comp_move == "paper")
+      @outcome = "won"
+    elsif (user_move == "rock" && @comp_move == "paper") || (user_move == "paper" && @comp_move == "scissors") || (user_move == "scissors" && @comp_move == "rock")
+      @outcome = "lost"
+    end
+  end
+
   def play_rock
+    self.outcome("rock")
     render({ :template => "game_templates/user_rock.html.erb" })
   end
 
   def play_paper
-    @comp_move = ["rock", "paper", "scissors"].sample
-
-    if @comp_move == "rock"
-      @outcome = "won"
-    elsif @comp_move == "paper"
-      @outcome = "tied"
-    elsif @comp_move == "scissors"
-      @outcome = "lost"
-    end
-
+    self.outcome("paper")
     render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    self.outcome("scissors")
+    render({ :template => "game_templates/user_scissors.html.erb" })
   end
 end
